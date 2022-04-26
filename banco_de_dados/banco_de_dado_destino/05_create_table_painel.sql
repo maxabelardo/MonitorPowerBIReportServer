b50b5c3bc3e0e41b0638f989e50c2b821e234ec7
José Abelardo Vicente Filho <maxabelardo@hotmail.com>
2022-04-26 17:43:01 -0300

    Create 05_create_table_painel.sql

---
+++ banco_de_dados/banco_de_dado_destino/05_create_table_painel.sql
@@ -0,0 +1,45 @@
+/*********************************************************************************************
+Script de criação da tabela "painel"
+
+Descrição:
+	A tabela "painel" deverá armazenar os painéis da organiza do site ou portal.
+
+Relacionamento:
+	se relaciona com a tabela pai "instancia" que será de N para 1.
+
+A tabela está sendo criada no filegroup "PRIMARY"
+
+
+*********************************************************************************************/
+
+CREATE TABLE [objects].[painel](
+	[idpainel] [int] IDENTITY(1,1) NOT NULL,
+	[idpasta] [int] NOT NULL,
+	[itemid] [nvarchar](40) NULL,
+	[parentid] [nvarchar](40) NULL,
+	[localizacao] [nvarchar](425) NULL,
+	[painel] [nvarchar](425) NULL,
+	[tipo] [varchar](20) NULL,
+	[datadacriacao] [datetime] NULL,
+	[datadamodificacao] [datetime] NULL,
+	[ultimavisualizacao] [datetime] NULL,
+	[diassemalteracao] [int] NULL,
+	[tamanho] [float] NULL,
+	[ativo] [bit] NULL,
+	[createdbyusername] [nvarchar](260) NULL,
+	[modifiedbyusername] [nvarchar](260) NULL,
+ CONSTRAINT [PK_idObjeto] PRIMARY KEY CLUSTERED 
+(
+	[idpainel] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [objects].[painel] ADD  DEFAULT ((1)) FOR [ativo]
+GO
+
+ALTER TABLE [objects].[painel]  WITH CHECK ADD  CONSTRAINT [FK_painel_pasta] FOREIGN KEY([idpasta])
+REFERENCES [objects].[pasta] ([idpasta])
+GO
+
+
